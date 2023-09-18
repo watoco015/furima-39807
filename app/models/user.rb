76219c50fbc0ac198d19567de6740d6d3e672d8d
watoco015 <fuwari015@gmail.com>
2023-09-18 12:38:37 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :email, presence: true
-         validates :encrypted_password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-         validates :family_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-         validates :family_name_kana, presence: true,format: { with: /\A[ァ-ヴー]+\z/u }
-         validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヴー]+\z/u }
+         validates :encrypted_password, presence: true,
+                   format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+         validates :family_name, presence: true,
+                   format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+         validates :first_name, presence: true, 
+                   format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+         validates :family_name_kana, presence: true,
+                   format: { with: /\A[ァ-ヴー]+\z/u }
+         validates :first_name_kana, presence: true,
+                   format: { with: /\A[ァ-ヴー]+\z/u }
          validates :birthday, presence: true
 end
