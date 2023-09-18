2ff0e839ae4f6c4a22207002b05b34f569fe0379
watoco015 <fuwari015@gmail.com>
2023-09-18 22:17:31 +0900

    アクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :category_id, presence: true
+  validates :note, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+
+
+
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
