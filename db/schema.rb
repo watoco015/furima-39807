4a1ac7913a40737d9c4ac407690326a206377f5c
watoco015 <fuwari015@gmail.com>
2023-09-18 22:14:46 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_093712) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_102019) do
   create_table "items", charset: "utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.bigint "user_id", null: false
+    t.string "item_name", null: false
+    t.integer "category_id", null: false
+    t.text "note", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_items_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_items_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -41,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_093712) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
