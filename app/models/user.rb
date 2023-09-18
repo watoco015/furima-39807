961439fd16fb473df283984bc967f1deafbae093
watoco015 <fuwari015@gmail.com>
2023-09-18 17:50:39 +0900

    passwordのpresence: true削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :password, presence: true,
+         validates :password,
                    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
          validates :family_name, presence: true,
                    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
