4e00b242b7a494e9b0d3c70644ae0e4f61a0f713
watoco015 <fuwari015@gmail.com>
2023-09-21 12:05:28 +0900

    価格表示修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,8 +1,12 @@
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
-    const addPofitDom = document.getElementById("profit");
-    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
-  });
+function callback(){
+const priceInput = document.getElementById("item-price");
+   priceInput.addEventListener("input", () => {
+     const addTaxDom = document.getElementById("add-tax-price");
+     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+     const addPofitDom = document.getElementById("profit");
+     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
+};
+
+window.addEventListener('load', callback)
+window.addEventListener('click', callback)
