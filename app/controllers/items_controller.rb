b54edf00d386de59ecf8a1889a17fe702ec006c0
watoco015 <fuwari015@gmail.com>
2023-09-22 21:23:12 +0900

    destroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,6 +35,11 @@ class ItemsController < ApplicationController
       else
         render :edit, status: :unprocessable_entity
       end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
