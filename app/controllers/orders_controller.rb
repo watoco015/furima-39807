0b62c7f4101fb00c7815520139936673967efe9d
watoco015 <fuwari015@gmail.com>
2023-09-30 23:26:55 +0900

    コントローラー追加

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+
+  def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @orderform = OrderForm.new
+  end
+
+  def create
+    @orderform = OrderForm.new(order_params)
+    if @orderform.valid?
+      pay_item
+      @orderform.save(params,current_user.id)
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number, :token).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def set_item
+    item_id = params[:item_id]
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
