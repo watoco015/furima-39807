d4626b5d41c1a1daecd0e539069a893dbb3d4c9a
watoco015 <fuwari015@gmail.com>
2023-10-01 15:15:21 +0900

    テストコード

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -5,11 +5,15 @@ class OrderForm
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :post_code,
+               format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid" }
+    validates :prefecture_id, 
+               exclusion: { in: [1], message: "must be other than 1" }
+
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+    validates :phone_number,
+               format: { with: /\A\d{10,11}\z/,message: "is invalid"}
     validates :token
   end
   def save(params,user_id)
